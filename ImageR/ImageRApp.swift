fb3408842e4d59a4b45e5878cd36d218cf12dfda
Tom Yan <143831712+tomyanzhiyuan@users.noreply.github.com>
2024-12-01 13:07:24 -0500

    Initial Commit

---
+++ ImageR/ImageRApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRApp.swift
+//  ImageR
+//
+//  Created by Tom Yan Zhiyuan on 01/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
